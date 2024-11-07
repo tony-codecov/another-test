6bacbbe0c268221cbd32122647f5be593b4f64ba
Tony Le <tony.le@sentry.io>
2024-11-06 22:44:46 -0500

    new subtract test

--- flag1/mafs.test.js
+++ flag1/mafs.test.js
@@ -4,8 +4,8 @@ test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
 });
 
-test("adds 1 + 7 to equal 8", () => {
-  expect(add(1, 7)).toBe(8);
+test("subtracts 5 - 1 to equal 4", () => {
+  expect(subtract(5, 1)).toBe(4);
 });
 
 test("adds 1 + 5 to equal 6", () => {
