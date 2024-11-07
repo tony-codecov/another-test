732ccb9c4fe161a7e9d744b00845b910f782e1ed
Tony Le <tony.le@sentry.io>
2024-11-07 12:36:31 -0500

    Increase coverage

--- flag1/mafs.test.js
+++ flag1/mafs.test.js
@@ -16,9 +16,9 @@ test("adds 1 + 5 to equal 6", () => {
 //     expect(subtract(4, 1)).toBe(3);
 // });....
 
-// // test('multiplies 5 * 6 to equal 30', () => {
-// //     expect(multiply(5, 6)).toBe(30);
-// // });
+test('multiplies 5 * 6 to equal 30', () => {
+    expect(multiply(5, 6)).toBe(30);
+});
 
 // // test('divides 6 / 0 to equal 0', () => {
 // //     expect(divide(6, 0)).toBe(0);
