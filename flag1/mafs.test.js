e18ebc42a4d416287ecc6c8a2ea8c98a4af35b37
Tony Le <tony.le@sentry.io>
2024-11-25 16:13:44 -0500

    no sum

--- flag1/mafs.test.js
+++ flag1/mafs.test.js
@@ -1,4 +1,4 @@
-const { add, subtract, multiply, divide, sum } = require("./mafs");
+const { add, subtract, multiply, divide } = require("./mafs");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
@@ -8,9 +8,9 @@ test('subtracts 4 - 1 to equal 3', () => {
     expect(subtract(4, 1)).toBe(3);
 });
 
-test('sums 1 + 2 + 3 + 4 to equal 10', () => {
-    expect(sum(1, 2, 3, 4)).toBe(10);
-});
+// test('sums 1 + 2 + 3 + 4 to equal 10', () => {
+//     expect(sum(1, 2, 3, 4)).toBe(10);
+// });
 
 test('multiplies 5 * 6 to equal 30', () => {
     expect(multiply(5, 6)).toBe(30);
