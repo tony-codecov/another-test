4b14c8985cdadc5763e55dcb79c6768595e615c0
Tony Le <tony.le@sentry.io>
2024-12-17 14:29:34 -0500

    remove add test

--- flag1/mafs.test.js
+++ flag1/mafs.test.js
@@ -1,8 +1,8 @@
 const { add, subtract, multiply, divide, sum } = require("./mafs");
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(add(1, 2)).toBe(3);
-});
+// test("adds 1 + 2 to equal 3", () => {
+//   expect(add(1, 2)).toBe(3);
+// });
 
 test('subtracts 4 - 1 to equal 3', () => {
     expect(subtract(4, 1)).toBe(3);
